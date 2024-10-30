251f1222ac5deffaf7497616a62c96d5f866dc9c
KURS HOMEWORK <kreshvan12317@yandex.ru>
2024-10-30 20:09:34 +0300

    ветка

---
+++ src/main/java/COURSEWORK/Dmitriev_COURSEWORK/DmitrievCourseworkApplication.java
@@ -0,0 +1,14 @@
+package COURSEWORK.Dmitriev_COURSEWORK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmitrievCourseworkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DmitrievCourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/COURSEWORK/Dmitriev_COURSEWORK/DmitrievCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package COURSEWORK.Dmitriev_COURSEWORK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmitrievCourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
